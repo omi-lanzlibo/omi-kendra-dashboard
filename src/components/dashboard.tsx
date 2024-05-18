e63c58655e210397ff526a82b588c8dcbe4a7277
omi-lanzlibo <lance.libo@smsupermalls.com>
2024-05-18 22:13:57 +0800

    TEST: "STAGING DEPLOYMENT"

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -108,7 +108,7 @@ export function DashboardScreen() {
         </header>
         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
           <div className="flex items-center">
-            <h1 className="text-lg font-semibold md:text-2xl">Kendra Dasboard</h1>
+            <h1 className="text-lg font-semibold md:text-2xl">Kendra Dasboard Staging</h1>
           </div>
           <div className="ml-6 flex  items-center gap-2">
                   <DropdownMenu>
