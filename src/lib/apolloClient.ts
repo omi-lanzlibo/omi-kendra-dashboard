a1090c8de9b177e3810a1a542462541abeab9f88
omi-lanzlibo <lance.libo@smsupermalls.com>
2024-10-15 12:18:14 +0800

    chore: update graphql .env.local

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -2,7 +2,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://develop.smop.asia/graphql/", // Replace with your actual GraphQL endpoint
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_API,
   cache: new InMemoryCache(),
 });
 
